b50598e463d24e5ea44bc8146083ca51a1d0d474
koreakoreakorea <162765672+koreakoreakorea@users.noreply.github.com>
2024-03-09 11:58:10 +0900

    20240309

---
+++ 001.py
@@ -0,0 +1,67 @@
+##a = 10
+##b = 2
+##print("%d %d %d" % (a + b, a - b, a * b))
+##print(7/2, 7//2, 7%2)
+##name = input("이름 : ")
+##age = int(input("나이 : "))
+##height = float(input("키 : "))
+
+##print("== 자기 소개하기 ==")
+##print("이름 : %s" % name)
+##print("나이 : %d" % age)
+##print("키 : %f" % height)
+##a = int(input("a 정수 입력 : "))
+##b = int(input("b 정수 입력 : "))
+##c = int(input("c 정수 입력 : "))
+
+##print("세 정수의 평균은 %0.2f입니다."% ((a+b+c)/3))
+##name = input("이름 : ")
+##age = int(input("나이 : "))
+##height = float(input("키 : "))
+
+##print("== 친구 소개 ==")
+##print("제 친구의 이름은 %s 이고, 나이는 %d 살 이에요.키는%.1f 입니다."%(name,age,height))
+##print("%10s %10d %10.2f" %('cat',3,3.14))
+##print("%10s %10d %10.3f" %('dog',127,1.234))
+##print("%10s %10d %10.1f" %('turtle',54321,10.1))
+##L = int(input("L 판매 개수 : "))
+##M = int(input("M 판매 개수 : "))
+##S = int(input("S 판매 개수 : "))
+
+##sum = 2500 * L
+##sum = sum + 1000 * M
+##sum = sum + 500 * S
+
+##print("오늘의 총 매출은 %d원 입니다."% sum)
+##height = int(input("height : "))
+##width = int(input("width : "))
+
+##print("area : %.1f" % (height*width/2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               
