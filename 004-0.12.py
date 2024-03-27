ce1e84d1f4f2d658397045819ce1cea777c82107
koreakoreakorea <162765672+koreakoreakorea@users.noreply.github.com>
2024-03-27 20:16:30 +0900

    Add files via upload
    2024-03-27 상상시간

---
+++ 004-0.12.py
@@ -0,0 +1,125 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+Hello
+world!
+Python is fun!
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+[]
+[1, 2, 3]
+['a', 'b', 'c']
+['hello', 'world']
+[1, 2, 'a', 'b', 'python']
+[1, 2, ['a', 'b', 'c'], 3]
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+Traceback (most recent call last):
+  File "C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py", line 19, in <module>
+    print(a + b)
+TypeError: can only concatenate str (not "list") to str
+>>> 
+
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+>>> 
+Traceback (most recent call last):
+  File "C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py", line 19, in <module>
+    print(a + b)
+TypeError: can only concatenate str (not "list") to str
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+Pythonis fun
+PythonPython
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+Traceback (most recent call last):
+  File "C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py", line 23, in <module>
+    print(a + 5)
+TypeError: can only concatenate list (not "int") to list
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+Traceback (most recent call last):
+  File "C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py", line 23, in <module>
+    print(a + 5)
+TypeError: can only concatenate list (not "int") to list
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+Traceback (most recent call last):
+  File "C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py", line 23, in <module>
+    print(a + 5)
+TypeError: can only concatenate list (not "int") to list
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+[1, 2, 3, 4, 5, 6]
+Traceback (most recent call last):
+  File "C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py", line 24, in <module>
+    print(a * b)
+TypeError: can't multiply sequence by non-int of type 'list'
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+[1, 2, 3, 4, 5, 6]
+[1, 2, 3, 1, 2, 3, 1, 2, 3]
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+['*']
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+Traceback (most recent call last):
+  File "C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py", line 33, in <module>
+    print(a)
+NameError: name 'a' is not defined
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+*
+***
+******
+***
+*
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+  *
+ ***
+******
+ ***
+ *
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+  *
+ ***
+******
+ ***
+  *
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+Diem!
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+Diem!
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+10
+[30, 40]
+30
+40
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+[20, 30, 40]
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+4
+[1, 2]
+[3, 4]
+3
+abc
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+['a', 20, 30, 40, 50]
+['a', 'b', 40, 50]
+>>> 
+============= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python38-32/004-0.1.py =============
+>>> 
