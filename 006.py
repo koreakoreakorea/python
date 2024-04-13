920ede9a822e1054dc005209f6b0cd4f827ac3da
koreakoreakorea <162765672+koreakoreakorea@users.noreply.github.com>
2024-04-13 11:52:10 +0900

    Add files via upload
    2024/4/13

---
+++ 006.py
@@ -0,0 +1,87 @@
+##score = 80
+##if score >= 60 and score <= 100 :
+    ##  print ("Pass")
+##else :
+     ## print("Fai")
+##x =10
+##print(x >= 0 and x <= 10)
+##print(x < 0 or x > 10)
+##print(not x)
+##a = int(input())
+##b = int(input())
+##c = int(input())
+##print( a >= 140 and b >= 140 and c >= 140)
+##score = int(input('score :  '))
+##if score >= 90 :
+   ##  print('A')
+##elif score >= 80 :
+ ##    print('B')
+##elif score >= 70 :
+  ##   print('C')
+##else   :
+    ## print('F')
+##nume = int(input('nume : '))
+##if  (nume % 2 == 0):
+    ##print('EVEN')
+##else  :
+    ##print('ODD')
+##y = int(input('year : '))
+
+##if (y % 4 == 0 and y % 100 !=0 or y % 400 == 0):
+   ##  print('leap year')
+##else :
+    ## print( ' common year')
+
+##b = input(' 성별을 입력 : ')
+##if (b == ('m')):
+    ## print('Man')
+##elif (b ==('w')):
+    ## print('Woman')
+##else  :
+    ## print('what')
+##import random
+
+##com = random.randint(1, 2)
+##user = int(input( 'odd : 1, even : 2\n'))
+
+##print('COM(%d)  :  USER(%d) ' %(com, user))
+##if com ==user :
+   ##  print('Correct')
+##else :
+    ## print('Incorrect')
+import random
+
+com = random.randint(1,3)
+user = int(input('1 : 가위, 2 : 바위, 3 : 보|n'))
+
+if com == 1:
+     com = '가위'
+elif com =2:
+     com = "바위"
+else:
+     com = "보"
+
+if user == 1:
+     user = '가위'
+elif user =2:
+     user = "바위"
+else:
+     user = "보"
+
+
+if user = com:
+     print('Com(%s) : User(%s)' % (com,user))
+     print("Draw")
+elif user == "가위"
+     if com =="바위"
+      print('Com(%s) : User(%s)' % (com,user))
+     print("Com Wins!")
+elif user =="보":
+
+
+
+
+     
+
+
+
